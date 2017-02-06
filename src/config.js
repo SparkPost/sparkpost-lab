c8e9ed4fb16bcb4322899e6370898c17d3fb5ccd
Avi Goldman <avrahamymgoldman@gmail.com>
2017-02-06 07:09:44 -0500

    changed prod url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let config = {
   'email_domain': 'challenge.sparkpost.com',
-  'api_base': process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'https://lab.sparkpost.com', 
+  'api_base': process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'http://lab.sparkpost.com', 
   'tracking_code': 'src=Dev-Website&sfdcid=701600000011daf'
 };
 
